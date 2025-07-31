4cd23558bc66e683504cbfd66589e3e72d0eff38
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-07-31 20:37:21 +0200

    añadido spinner mientras se recibe la request de la api

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -5,12 +5,12 @@ import PostDashboard from "../components/PostDashboard";
 
 const Posts = () => {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start",justifySelf: "flex-start", alignItems: "center" }}>
       <h1>Posts</h1>
       <NavButton text="Go to Home" route="/" />
       {/* <PostDashboard /> */}
       <PaginatedPostDashboard />
-    </>
+    </div>
   );
 };
 
