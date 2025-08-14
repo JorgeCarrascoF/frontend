48ad112ebcb63ae74606fa19ff2bdb6678174744
Frrambuessa <andresguaipo.16@gmail.com>
2025-08-14 01:08:18 -0400

    Adaptación de la pagina crear un log ux/ui

--- src/routes/LogCreatePage.jsx
+++ src/routes/LogCreatePage.jsx
@@ -2,9 +2,14 @@ import CreateLogForm from "../components/CreateLogForm";
 
 const LogCreatePage = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-8">Crear nuevo log</h1>
-      <CreateLogForm />
+    <div className=" w-[72%] h-[82%] mt-10">
+      <div className="flex flex-col text-left ms-5 mb-10">
+        <h1 className="mb-3 text-4xl">Crea un nuevo log</h1>
+        <h1 className="ms-1 text-m text-gray-500">Manualmente, introduce el contenido del nuevo log.</h1>
+      </div>
+      <div className="flex flex-col items-center m-2.5 rounded-2xl border-[1px] border-gray-200 bg-white h-[90%] py-3 px-6">
+        <CreateLogForm />
+      </div>
     </div>
   );
 };
