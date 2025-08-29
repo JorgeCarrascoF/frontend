a45f429e9b95aa75fa5602e89f27d4b5491e5226
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-08-29 21:47:12 +0200

    fix(userIcon): arreglado contraste en el icono de usuario

--- src/components/UserIconVerySmall.jsx
+++ src/components/UserIconVerySmall.jsx
@@ -1,6 +1,8 @@
 import { getColorForName } from "../utils/getColorForName";
 import { getInitials } from "../utils/getInitials";
 
+const lightColors = ["#F3F6FC", "#E6EDF8", "#96B7E3"];
+
 const UserIconVerySmall = ({ name = "Default User" }) => {
   const initials = getInitials(name);
   const bgColor = getColorForName(name);
@@ -9,7 +11,11 @@ const UserIconVerySmall = ({ name = "Default User" }) => {
       className={`rounded-full h-full flex items-center justify-center w-[30px] aspect-square`}
       style={{ backgroundColor: bgColor }}
     >
-      <span className="text-white font-bold select-none text-xs">
+      <span
+        className={`${
+          lightColors.includes(bgColor) ? "text-[#1F365B]" : "text-white"
+        }  font-bold select-none text-xs`}
+      >
         {initials}
       </span>
     </div>
